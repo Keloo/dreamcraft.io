a2b8487dfd8f8a1344bb7e542e4d16c8fdfecb04
Vlad Negura <vlad-negura@mail.ru>
2019-11-26 21:10:41 +0100

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -61,6 +61,15 @@ const loadSavedTheme = () => {
 const updateTheme = (theme) => {
   if (!isObject(theme)) return
 
+  const currentTheme = localStorage['currentTheme']
+  if (currentTheme === NightTheme.themeName) {
+    document.getElementById('logo_b').style.display = "block"
+    document.getElementById('logo_w').style.display = "none"
+  } else {
+    document.getElementById('logo_b').style.display = "none"
+    document.getElementById('logo_w').style.display = "block"
+  }
+  
   Object
   .entries(theme)
   .forEach(([key, value]) => setCSSVariable(key, value))
